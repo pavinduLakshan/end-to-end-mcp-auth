6109dac878cd5e08af50fbb1297a54c241a234f1
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-05-25 19:35:16 +0530

    Add asgardeo auth

--- mcp-client-react/src/main.tsx
+++ mcp-client-react/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from '@asgardeo/auth-react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+     <AuthProvider
+      config={ {
+        signInRedirectURL: 'http://localhost:5173',
+        signOutRedirectURL: 'http://localhost:5173',
+        clientID: 'iYt9lvROrGJ_0E2M5gSqNp5PjEUa',
+        baseUrl: 'https://api.asgardeo.io/t/pavinduorg',
+        scope: ['openid', 'profile'],
+      } }
+    >
+      <App />
+    </AuthProvider>
+  </StrictMode>
 )
